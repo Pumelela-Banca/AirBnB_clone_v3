147967428cec512c3df70f757d5c6b24307ecb35
Pumelela-Banca <pumelelabanca@gmail.com>
2023-10-28 18:05:34 +0200

    function returns all stats

--- api/v1/views/index.py
+++ api/v1/views/index.py
@@ -4,6 +4,7 @@ Creates a route for status
 """
 from flask import jsonify
 from api.v1.views import app_views
+from models import storage
 
 
 @app_views.route("/status")
@@ -12,3 +13,19 @@ def status():
     Returns json
     """
     return jsonify({"status": "OK"})
+
+
+@app_views.route("/api/v1/stats")
+def statistic():
+    """
+    Returns count for all classes
+    """
+
+    cls_dict = {
+            "amenities": storage.count("Amenity"),
+            "cities": storage.count("City"),
+            "places": storage.count("Place"),
+            "reviews": storage.count("Review"),
+            "states": storage.count("State"),
+            "users": storage.count("User")}
+    return jsonify(cls_dict)
