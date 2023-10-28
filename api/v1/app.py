20cd01ac6c890377f5257484f3ad1e430ba9cb99
Julesntizimira <ntizimijules5@gmail.com>
2023-10-28 14:31:34 +0000

    update task3

--- api/v1/app.py
+++ api/v1/app.py
@@ -4,14 +4,15 @@
 
 
 """
-
+import sys
+sys.path.append('../..')
 from os import environ
 from flask import Flask
 from models import storage
 from api.v1.views import app_views
 
 
-app =  Fask(__name__)
+app =  Flask(__name__)
 app.register_blueprint(app_views)
 
 
@@ -28,10 +29,10 @@ if __name__ == "__main__":
     if environ.get("HBNB_API_HOST"):
         _host = environ.get("HBNB_API_HOST")
     else:
-        _host = 0.0.0.0
+        _host = '0.0.0.0'
     if environ.get("HBNB_API_PORT"):
         _port = environ.get("HBNB_API_PORT")
     else:
-        _port = environ.get("HBNB_API_PORT")
+        _port = '5000'
     
     app.run(host=_host, port=_port, threaded=True)
