e56fc3daa1e2233ce454084c8020679a762acc5c
Julesntizimira <ntizimijules5@gmail.com>
2023-10-28 18:38:02 +0000

    task6

---
+++ api/v1/views/states.py
@@ -0,0 +1,71 @@
+#!/usr/bin/python3
+"""
+create a route for states
+"""
+from flask import jsonify, make_response, request
+from api.v1.views import app_views
+from models import storage
+from models.state import State
+
+
+@app_views.route("/states")
+def retrieve_all_state():
+    '''Retrieve all states objects in storage'''
+    all_state_list = []
+    for k, v in storage.all(State).items():
+        all_state_list.append(v.to_dict())
+    return jsonify(all_state_list)
+
+
+@app_views.route("/states/<state_id>")
+def retrieve_state(state_id):
+    '''retrieve a specific object'''
+    if storage.get('State', state_id):
+        return jsonify(storage.get('State', state_id).to_dict())
+    else:
+        return make_response(jsonify({'error': "Not found"}), 404)
+
+
+@app_views.route("/states/<state_id>", methods=["DELETE"])
+def delete_state(state_id):
+    '''delete a state object'''
+    if storage.get('State', state_id):
+        storage.delete(storage.get('State', state_id))
+        storage.save()
+        return jsonify({}), 200
+    else:
+        return make_response(jsonify({'error': "Not found"}), 404)
+
+
+@app_views.route("/states/", methods=["POST"])
+def add_state():
+    '''add a state object'''
+    try:
+        data = request.get_json()
+    except Exception:
+        return make_response(jsonify("Not a JSON"), 404)
+    if not data or 'name' not in data:
+        return make_response(jsonify("Missing name"), 404)
+    else:
+        new_state = State(name=data.get('name'))
+        storage.new(new_state)
+        storage.save()
+        return make_response(jsonify(new_state.to_dict()), 201)
+
+
+@app_views.route("/states/<state_id>", methods=["PUT"])
+def update_state(state_id):
+    '''update a state object'''
+    if not storage.get('State', state_id):
+        return make_response(jsonify(""), 404)
+    else:
+        st = storage.get('State', state_id)
+    try:
+        data = request.get_json()
+    except Exception:
+        return make_response(jsonify("Not a JSON"), 404)
+    for att, val in data.items():
+        if att != 'id' and att != 'created_at' and att != 'updated_at':
+            setattr(st, att, val)
+    storage.save()
+    return make_response(jsonify(st.to_dict()), 200)
