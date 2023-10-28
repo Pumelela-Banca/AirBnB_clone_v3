ac0361f4f534bc02c1f959a9ffc69a3398728add
Julesntizimira <ntizimijules5@gmail.com>
2023-10-28 16:36:09 +0000

    task5

--- api/v1/app.py
+++ api/v1/app.py
@@ -3,13 +3,19 @@
 create a flask app
 """
 from os import environ
-from flask import Flask
+from flask import Flask, jsonify, make_response
 from api.v1.views import app_views
 from models import storage
 
 
 app = Flask(__name__)
 app.register_blueprint(app_views)
+
+
+@app.errorhandler(404)
+def error_handler(error):
+    """ custom error 404 error handler method"""
+    return make_response(jsonify({'error': "Not found"}), 404)
 
 
 @app.teardown_appcontext
