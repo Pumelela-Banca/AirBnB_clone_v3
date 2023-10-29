eeecc0b84964d484ae291c94a4959d93e577bee1
Pumelela-Banca <pumelelabanca@gmail.com>
2023-10-29 15:49:43 +0200

    added got /users

--- api/v1/views/users.py
+++ api/v1/views/users.py
@@ -8,3 +8,16 @@ from models import storage
 from models.user import User
 
 
+@app_views.route('/users', methods=['GET'], strict_slashes=False)
+def get_user():
+    """
+    Gets all users
+    """
+    all_users = storage.all(User).values()
+    list_all_users = []
+    for user in all_users:
+        list_all_users.append(user.to_dict())
+
+    return jsonify(list_all_users)
+
+
